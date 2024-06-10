9d22d2afee94dacfed4c4932bceafe797870cf72
Phittawat Champajan <phittawat@Phittawats-MacBook-Air.local>
2024-06-10 08:51:58 +0700

    Add Firebaseconfig.js

---
+++ js/firebaseConfig.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyDxJrVpuV1FTOkzFPQ_dXNdyDm9tzdu_mA",
+    authDomain: "peane3mtrv.firebaseapp.com",
+    projectId: "peane3mtrv",
+    storageBucket: "peane3mtrv.appspot.com",
+    messagingSenderId: "242507253259",
+    appId: "1:242507253259:web:32dbfcf2e5962e101e290e",
+    measurementId: "G-DHKMK6GHV6"
+};
